4676a6d24ff7fca7d264063076290c57567acba7
John <john.m.zie@gmail.com>
2017-12-21 09:38:23 -0600

    Changed method in xmlReader to be temperature specific, and trimmed off the start and end tags from the string it returns

--- src/com/zielinski/weather/XMLReader.java
+++ src/com/zielinski/weather/XMLReader.java
@@ -5,21 +5,31 @@ import java.net.URL;
 import java.io.*;
 
 public class XMLReader {
+	//Main for testing
 	public static void main(String[] args) {
-		System.out.println(readXMLFeed("http://w1.weather.gov/xml/current_obs/KSPI.xml"));
+		System.out.println("It is " + readXMLFeedTemperature("http://w1.weather.gov/xml/current_obs/KSPI.xml") + " in Springfield, IL.");
 	}
-	public static String readXMLFeed(String urlAddress) {
+	
+	/*
+	 * Reads the XML at the given URL
+	 * Returns a string containing the temperature in degrees F and C
+	 */
+	public static String readXMLFeedTemperature(String urlAddress) {
 		try {
-			String sourceCode = "";
+			String temperatureStr = "";
 			String line;
+			String targetTagStart = "<temperature_string>";
+			String targetTagEnd = "</temperature_string>";
 			URL xmlUrl = new URL (urlAddress);
 			BufferedReader in = new BufferedReader(new InputStreamReader(xmlUrl.openStream()));
 			while((line = in.readLine()) != null) {
-				if(line.contains("<temperature_string")) {
-					System.out.println(line);
+				if(line.contains(targetTagStart)) {
+					int firstPos = line.indexOf(targetTagStart);
+					int lastPos = line.indexOf(targetTagEnd);
+					temperatureStr = line.substring(firstPos + targetTagStart.length(), lastPos);
+					return temperatureStr;
 				}
 			}
-			return sourceCode;
 		} catch (MalformedURLException ue) {
 			System.out.println("Malformed URL");
 		} catch (IOException ioe) {
